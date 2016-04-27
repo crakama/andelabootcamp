7d74a269458fcc94d3fa2d2f87e5d30e7dd494bf
crakama <crakama89@gmail.com>
2016-04-27 17:44:37 +0300

    OOP: Classes and import modules

--- Day_3/oop.py
+++ Day_3/oop.py
@@ -1,27 +1,17 @@
-class Person:
-	# class variable
-	people_count = 0
-	def __init__(self, name, age):
-		# instance variable to access a class varaible you use the name of the class 
-		# to use an instance varaible you use
-		# can access class variable thru instance
+from person import Person
+from kenyan import Kenyan
 
-		self.name = name
-		self.age= age
-		Person.people_count  += 1
-	def __repr__(self):
-		return "<Object: {},  {}>".format(self.name, self.age)
-
-		print "Object created"
-	def say_hello(self):
-		return "Hello, I'm {}  and I'm {} y/o".format(self.name,self.age)
+# ASIDE:
+# Read about:
+# PEP8
+# Instance variables vs class varaibles
 
 b = []
 
 p = Person('joe', 23)
 p2 = Person('felix', 23)
-# print p.name
-# print p.age
+print p.name
+print p.age
 print p2.people_count
 print p.say_hello()
 
@@ -31,3 +21,8 @@ for name, age in a:
 	b.append(person)
 	print b
 
+k = Kenyan('Anita Wanguru', 20)
+# k.probe(True)
+print "Is {} corrupt? {}".format(k.name, k.is_corrupt())
+print k.say_hello()
+		
