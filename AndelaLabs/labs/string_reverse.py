8285f35f97aa5216237462c3f14a73f66bdd20bd
crakama <crakama89@gmail.com>
2016-04-28 03:32:52 +0300

    Add String Reverse Solution

---
+++ AndelaLabs/labs/string_reverse.py
@@ -0,0 +1,14 @@
+def reverse_string(string):
+	"""
+	This function takes in any string and returns the reverse
+
+	"""
+
+  if string == "":
+    
+     return None
+  else:
+    
+    new_str = string[::-1]
+    
+    return new_str
